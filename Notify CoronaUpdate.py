3e59ac20f8417e6a899faf70524795ef4d7056b5
samarthshete <46786711+samarthshete@users.noreply.github.com>
2021-09-25 13:50:43 +0530

    Add files via upload

---
+++ Notify CoronaUpdate.py
@@ -0,0 +1,8 @@
+from plyer import notification
+
+notification.notify(
+    title='Corona Updates',
+    message='Here it is',
+    app_icon="F:\Python\Python_Project\Corona update notification\Corona.ico",
+    timeout=10,
+)
